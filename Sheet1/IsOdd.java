1e43e2ef3ef05d05faa0e78f838ca93eae7554e6
harshit <harshitsinghgla2415@gmail.com>
2025-11-16 19:18:57 +0530

    IsOdd

---
+++ Sheet1/IsOdd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class IsOdd {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int num = input.nextInt();
+		if(num < 0) {
+			num *= (-1);
+		}
+		if(num > 0 && num % 2 != 0) {
+			System.out.print(2);;
+		}else {
+			System.out.print(1);
+		}
+
+	}
+
+}
